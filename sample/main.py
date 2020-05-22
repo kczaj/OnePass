0e0b0b8d3b7f3dcebe0902330db089d69057128f
czajkaj <01142557@pw.edu.pl>
2020-05-22 09:51:13 +0200

    Poprawienie wyglądu przycisków

--- sample/main.py
+++ sample/main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     hasher = MHasher()
     print(hasher.hash('passwrd12'))
     encryptor = MEncryptor()
-    encryptor.decrypt('dtfygui')
+    #encryptor.decrypt('dtfygui')
     '''salt = get_random_bytes(32)
     file = open('data/logins', 'r')
     msg = file.read()
