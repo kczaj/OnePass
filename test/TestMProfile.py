09c01be2a0a0de2e4d5da7d18319c18e2d501d29
czajkaj <01142557@pw.edu.pl>
2020-05-30 21:43:41 +0200

    Dodanie testów do klasy MProfile.py

--- test/TestMProfile.py
+++ test/TestMProfile.py
@@ -1,22 +1,60 @@
 import unittest
 
+from sample.model.MProfile import MProfile
+
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.profile = MProfile('kuba', 'c', 'dwqd@wfaf.pl', 'test', 'Testtttt21')
+
     def test_update_all_good(self):
-        self.assertEqual(True, False)
+        name = 'pwoq'
+        surname = 'jdiwoq'
+        email = 'iuhwqd@qwjfqf.pl'
+        data = (name, surname, email)
+        exp = 1;
+        result = self.profile.update(data)
+        self.assertEqual(exp, result)
 
     def test_update_wrong_name(self):
-        self.assertEqual(True, False)
+        name = 'pwoq123'
+        surname = 'jdiwoq'
+        email = 'iuhwqd@qwjfqf.pl'
+        data = (name, surname, email)
+        exp = -1001;
+        result = self.profile.update(data)
+        self.assertEqual(exp, result)
 
     def test_update_wrong_surname(self):
-        self.assertEqual(True, False)
+        name = 'pwoq'
+        surname = 'jdiwoq2141421'
+        email = 'iuhwqd@qwjfqf.pl'
+        data = (name, surname, email)
+        exp = -1002;
+        result = self.profile.update(data)
+        self.assertEqual(exp, result)
 
     def test_update_wrong_email(self):
-        self.assertEqual(True, False)
+        name = 'pwoq'
+        surname = 'jdiwoq'
+        email = 'iuhwqdqwjfqf.pl'
+        data = (name, surname, email)
+        exp = -1003;
+        result = self.profile.update(data)
+        self.assertEqual(exp, result)
 
-    def test_update_password(self):
-        self.assertEqual(True, False)
+    def test_update__wrong_password(self):
+        password = 'drt1'
+        exp = False
+        result = self.profile.update_password(password)
+        self.assertEqual(exp, result)
+
+    def test_update__good_password(self):
+        password = 'Lowskwqi76'
+        exp = True
+        result = self.profile.update_password(password)
+        self.assertEqual(exp, result)
 
 
 if __name__ == '__main__':
