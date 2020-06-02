76112f5d07edcca30f465f304f5cc1404db6017a
czajkaj <01142557@pw.edu.pl>
2020-06-02 14:37:39 +0200

    Poprawienie plików

---
+++ sample/__main__.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5 import QtWidgets, QtGui
+
+from sample.model.MEncryptor import MEncryptor
+from sample.model.MHasher import MHasher
+from sample.view.VMainWinBefore import VMainWinBefore
+
+
+if __name__ == '__main__':
+    hasher = MHasher()
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = QtWidgets.QMainWindow()
+    w = VMainWinBefore(main_window, hasher)
+    sys.exit(app.exec_())
